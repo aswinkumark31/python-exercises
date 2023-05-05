f89fc0401cdccb93ec849db111e9fe0905ee1ebc
aswin <aswinsynnefo3132001@gmail.com>
2023-05-05 13:14:55 +0530

    file added

---
+++ methodoverriding.py
@@ -0,0 +1,17 @@
+
+# ENCAPSULATION
+
+# class school:
+#     b= 5
+#     __a = 10 
+#     def display(self):
+#         s = self.__a + 10
+#         print(s)
+
+# ob = school()                    
+# ob.display()
+# print(ob.b)
+# print(ob._school__a)
+
+
+
